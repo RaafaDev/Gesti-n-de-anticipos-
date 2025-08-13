73f54d24e74bf5697066423c826c3731109f2315
Laura Porras <laura.porras@crowe.com.co>
2025-08-13 08:53:05 -0500

    Crud Modulo de contratos, crear contratos.

---
+++ Controllers/ContratoesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GestionAnticiposApp.Data;
+using GestionAnticiposApp.Models;
+
+namespace GestionAnticiposApp.Controllers
+{
+    public class ContratoesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContratoesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Contratoes
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Contratos.Include(c => c.Persona);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Contratoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contrato = await _context.Contratos
+                .Include(c => c.Persona)
+                .FirstOrDefaultAsync(m => m.ContratoId == id);
+            if (contrato == null)
+            {
+                return NotFound();
+            }
+
+            return View(contrato);
+        }
+
+        // GET: Contratoes/Create
+        public IActionResult Create()
+        {
+            ViewData["PersonaId"] = new SelectList(_context.Personas, "PersonaId", "PersonaId");
+            return View();
+        }
+
+        // POST: Contratoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ContratoId,Codigo,Estado,Servicio,FechaInicio,FechaFin,Empresa,Referencia,PersonaId")] Contrato contrato)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contrato);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PersonaId"] = new SelectList(_context.Personas, "PersonaId", "PersonaId", contrato.PersonaId);
+            return View(contrato);
+        }
+
+        // GET: Contratoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contrato = await _context.Contratos.FindAsync(id);
+            if (contrato == null)
+            {
+                return NotFound();
+            }
+            ViewData["PersonaId"] = new SelectList(_context.Personas, "PersonaId", "PersonaId", contrato.PersonaId);
+            return View(contrato);
+        }
+
+        // POST: Contratoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ContratoId,Codigo,Estado,Servicio,FechaInicio,FechaFin,Empresa,Referencia,PersonaId")] Contrato contrato)
+        {
+            if (id != contrato.ContratoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contrato);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContratoExists(contrato.ContratoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PersonaId"] = new SelectList(_context.Personas, "PersonaId", "PersonaId", contrato.PersonaId);
+            return View(contrato);
+        }
+
+        // GET: Contratoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contrato = await _context.Contratos
+                .Include(c => c.Persona)
+                .FirstOrDefaultAsync(m => m.ContratoId == id);
+            if (contrato == null)
+            {
+                return NotFound();
+            }
+
+            return View(contrato);
+        }
+
+        // POST: Contratoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var contrato = await _context.Contratos.FindAsync(id);
+            if (contrato != null)
+            {
+                _context.Contratos.Remove(contrato);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContratoExists(int id)
+        {
+            return _context.Contratos.Any(e => e.ContratoId == id);
+        }
+    }
+}
